d7962b6dcc85862229699dcd13c7c7ae263d59cd
Liz P <parisielizabeth@gmail.com>
2021-03-21 03:54:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,7 +28,70 @@ function getToys() {
   });
 }
 
+function renderToys(toy) {
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
 
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.setAttribute('class', 'toy-avatar')
+
+  let p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = "Like <3"
+  btn.addEventListener('click', (e) => {
+    console.log(e.target.dataset);
+    likes(e)
+  })
+
+  let divCard = document.createElement('div')
+  divCard.setAttribute('class', 'card')
+  divCard.append(h2, img, p, btn)
+  toyColl.append(divCard)
+} 
+
+function postToy(toy_data) {
+  fetch(allToys, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": toy_data.name.value,
+        "image": toy_data.image.value,
+        "likes": 0
+      })
+    })
+    .then(res => res.json())
+    .then(obj_toy => renderToys(obj_toy))
+}
+
+function likes(e) {
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText) + 1
+
+  fetch(`${allToys}/${e.target.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "likes": more
+      })
+    })
+    .then(res => res.json())
+    .then((like_obj => {
+      e.target.previousElementSibling.innerText = `${more} likes`;
+    }))
+}
+
+getToys()
 
 // function getNewToy() {
 //   const realForm = document.querySelector('.add-toy-form')
