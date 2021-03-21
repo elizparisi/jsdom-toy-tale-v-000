dd0c457827510bee495c1286069002f912b37835
Liz P <parisielizabeth@gmail.com>
2021-03-21 03:54:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,85 +28,87 @@ function getToys() {
   });
 }
 
-function getNewToy() {
-  const realForm = document.querySelector('.add-toy-form')
-    realForm.addEventListener("submit", function(e) {
-      e.preventDefault()
 
-      let name = e.target[0].value 
-      let image = e.target[1].value
 
-      fetch("http://localhost:3000/toys", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: "application/json"
-        },
-        body: JSON.stringify({
-          "name": name,
-          "image": image,
-          "likes": 0
-          })
-        })
+// function getNewToy() {
+//   const realForm = document.querySelector('.add-toy-form')
+//     realForm.addEventListener("submit", function(e) {
+//       e.preventDefault()
 
-      .then(response => response.json())
-      .then(newToy => createCard(newToy))
-    })
-}
+//       let name = e.target[0].value 
+//       let image = e.target[1].value
 
-function createCard(toy) {
-  let c = document.createElement("div")
-  c.className = "card";
-  c.id = toy.id
-  let name = document.createElement("h2")
-  name.innerText = toy.name
-  let image = document.createElement("img")
-  image.className = "toy-avatar"
-  image.src = toy.image
-  let likes = document.createElement("p")
-  likes.innerHTML = toy.likes + " likes"
-  let button = document.createElement("button")
-  button.className = "like-btn"
-  button.innerHTML = "Like <3"
-  button.dataset.toyid = toy.id
-  c.append(name, image, likes, button)
-  const toyColl = document.getElementById("toy-collection");
-  toyColl.append(c)
-};
+//       fetch("http://localhost:3000/toys", {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           Accept: "application/json"
+//         },
+//         body: JSON.stringify({
+//           "name": name,
+//           "image": image,
+//           "likes": 0
+//           })
+//         })
 
-function increaseLikes() {
+//       .then(response => response.json())
+//       .then(newToy => createCard(newToy))
+//     })
+// }
+
+// function createCard(toy) {
+//   let c = document.createElement("div")
+//   c.className = "card";
+//   c.id = toy.id
+//   let name = document.createElement("h2")
+//   name.innerText = toy.name
+//   let image = document.createElement("img")
+//   image.className = "toy-avatar"
+//   image.src = toy.image
+//   let likes = document.createElement("p")
+//   likes.innerHTML = toy.likes + " likes"
+//   let button = document.createElement("button")
+//   button.className = "like-btn"
+//   button.innerHTML = "Like <3"
+//   button.dataset.toyid = toy.id
+//   c.append(name, image, likes, button)
+//   const toyColl = document.getElementById("toy-collection");
+//   toyColl.append(c)
+// };
+
+// function increaseLikes() {
   
-  let toyId = toy.id;
-  let likes = toy.likes;
-  let newLikes = likes + 1 ;
+//   let toyId = toy.id;
+//   let likes = toy.likes;
+//   let newLikes = likes + 1 ;
 
   
-  fetch(`http://localhost:3000/toys/${toyId}`), {
-    method: "PATCH",
-    headers: 
-    {
-    "Content-Type": "application/json",
-    Accept: "application/json"
-    },
+//   fetch(`http://localhost:3000/toys/${toyId}`), {
+//     method: "PATCH",
+//     headers: 
+//     {
+//     "Content-Type": "application/json",
+//     Accept: "application/json"
+//     },
 
-    body: JSON.stringify({
-    "likes": newLikes
-    })
-  }
-}
+//     body: JSON.stringify({
+//     "likes": newLikes
+//     })
+//   }
+// }
 
 
-function increaseLikes(toy, numberOfLikes){
-  let toyUrl = `http://localhost:3000/toys/${toy.id}`;
+// function increaseLikes(toy, numberOfLikes){
+//   let toyUrl = `http://localhost:3000/toys/${toy.id}`;
   
-  let obj = {
-      method: 'PATCH',
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({
-        "likes": toy.likes + 1
-      })
-    }
-}
+//   let obj = {
+//       method: 'PATCH',
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify({
+//         "likes": toy.likes + 1
+//       })
+//     }
+// }
